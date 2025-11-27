c11e198add8faf2b0fc62bc972d11fdc32bf44b2
Bang2704 <khanhbangvt123@gmail.com>
2025-11-27 09:48:55 +0700

    remove test_gemini

--- test_gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA7OTmiUR_aIsUDZrNUjEMp_7AHX3HBDsE")
-
-print("List of available models:")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
